679e7c692fda67a10e0f8a821f0091d16b593f0a
ZHANGSM <mataszhang@163.com>
2018-08-02 19:21:48 +0800

    session

--- src/main/java/com/matas/springbootdemo/controller/TestController2.java
+++ src/main/java/com/matas/springbootdemo/controller/TestController2.java
@@ -2,8 +2,12 @@ package com.matas.springbootdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author matas
@@ -13,14 +17,17 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @Controller
 @SessionAttributes("name")
 public class TestController2 {
-    @RequestMapping("hello")
+    @RequestMapping("set")
     public String hello(Model model) {
         model.addAttribute("name", "jack");
-        return "hello";
+        HttpServletRequest request = null;
+        return "set";
     }
 
     @RequestMapping("session")
-    public String session(String name) {
-        return "hello";
+    public String session(@ModelAttribute("name") String nameInModel, @SessionAttribute("name") String nameInSession, Model model) {
+        model.addAttribute("nameInModel", nameInModel);
+        model.addAttribute("nameInSession", nameInSession);
+        return "session";
     }
 }
